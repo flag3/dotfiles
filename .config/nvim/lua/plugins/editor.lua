3316132e9c3484cac998c9dd4be04b0c325e9f05
flag3 <flag3833753@gmail.com>
2024-12-21 12:54:39 +0900

    fix: 🐛 disable scroll animations

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -218,4 +218,23 @@ return {
 			require("telescope").load_extension("bibtex")
 		end,
 	},
+
+	{
+		"saghen/blink.cmp",
+		opts = {
+			completion = {
+				menu = {
+					winblend = vim.o.pumblend,
+				},
+				documentation = {
+					winblend = vim.o.pumblend,
+				},
+			},
+			signature = {
+				window = {
+					winblend = vim.o.pumblend,
+				},
+			},
+		},
+	},
 }
