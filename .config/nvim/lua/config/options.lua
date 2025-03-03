a3c50d1ea348c78a83209b9053a862a557a6c041
flag3 <flag3833753@gmail.com>
2025-03-03 17:11:50 +0900

    fix: 🐛 options

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -50,4 +50,14 @@ if vim.fn.has("nvim-0.8") == 1 then
 	vim.opt.cmdheight = 0
 end
 
+-- File types
+vim.filetype.add({
+	extension = {
+		mdx = "mdx",
+	},
+})
+
+vim.g.lazyvim_prettier_needs_config = true
+vim.g.lazyvim_picker = "telescope"
+vim.g.lazyvim_cmp = "blink.cmp"
 vim.g.vimtex_view_method = "skim"
