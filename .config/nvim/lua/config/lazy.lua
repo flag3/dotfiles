4ffd5cea4812f1e848a06c99daac5f0a418729e5
flag3 <flag3833753@gmail.com>
2025-09-30 02:03:00 +0900

    feat: add TOML language support to Neovim LazyVim config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -33,6 +33,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.terraform" },
 		{ import = "lazyvim.plugins.extras.lang.tex" },
+		{ import = "lazyvim.plugins.extras.lang.toml" },
 		{ import = "lazyvim.plugins.extras.lang.vue" },
 		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		-- { import = "lazyvim.plugins.extras.dap.core" },
